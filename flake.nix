e689fa39f0332b8a73550e5ad522a6b7aba90406
aabccd021 <aabccd021@gmail.com>
2025-02-28 07:49:57 +0700

    checkpoint

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  nixConfig.allow-import-from-derivation = false;
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
+    let
+
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.nixpkgs-fmt.enable = true;
+        programs.prettier.enable = true;
+        programs.shfmt.enable = true;
+        programs.shellcheck.enable = true;
+        settings.formatter.shellcheck.options = [ "-s" "sh" ];
+        settings.global.excludes = [ "LICENSE" "generated/**" ];
+      };
+
+      generated = import ./generated {
+        pkgs = pkgs;
+        system = "x86_64-linux";
+        nodejs = pkgs.nodejs;
+      };
+
+      updateDependencies = pkgs.writeShellApplication {
+        name = "update-dependencies";
+        text = ''
+          trap 'cd $(pwd)' EXIT
+          root=$(git rev-parse --show-toplevel)
+          cd "$root" || exit
+          git add -A
+          trap 'git reset >/dev/null' EXIT
+
+          ${pkgs.nodejs}/bin/npm install --lockfile-version 2 --package-lock-only
+
+          cd generated
+          ${pkgs.node2nix}/bin/node2nix -- --input ../package.json --lock ../package-lock.json
+        '';
+      };
+
+      packages = {
+        formatting = treefmtEval.config.build.check self;
+        # tailwindcss = tailwindcss;
+        default = generated.package;
+      };
+
+      gcroot = packages // {
+        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+      };
+
+    in
+    {
+      packages.x86_64-linux = gcroot;
+      checks.x86_64-linux = gcroot;
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      apps.x86_64-linux.fix = {
+        type = "app";
+        program = "${updateDependencies}/bin/update-dependencies";
+      };
+
+    };
+}
