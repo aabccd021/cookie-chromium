12842ac5b09b8bdc22955d819ec44666281d537c
aabccd021 <aabccd021@gmail.com>
2025-05-21 21:00:48 +0700

    Revert "feat: Enable strict mode for nixfmt"
    This reverts commit 9b6dfdcb93aada6f92ca09db9f89481ed29d897a.

--- flake.nix
+++ flake.nix
@@ -35,7 +35,6 @@
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.biome.enable = true;
         programs.shfmt.enable = true;
